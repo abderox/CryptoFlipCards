a722fe12892ef400f83535f054d6c282cecba1de
abderox <abdelhadi12mouzafir@gmail.com>
2022-05-15 13:52:55 +0100

    deploy to github

--- flipCards/vite.config.js
+++ flipCards/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CryptoFlipCards/flipCards',
+  base: '/CryptoFlipCards/flipCards/',
   plugins: [react()],
   server: {
     host: true
