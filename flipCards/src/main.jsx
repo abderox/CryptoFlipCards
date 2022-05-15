24ffe4980cc4a5dda8bc1a7254ce397044dc22e1
MOUZAFIR <81394980+abderox@users.noreply.github.com>
2022-05-15 13:37:13 +0100

    Update main.jsx

--- flipCards/src/main.jsx
+++ flipCards/src/main.jsx
@@ -5,11 +5,5 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </HashRouter>
-  </React.StrictMode>
+  <App />
 )
